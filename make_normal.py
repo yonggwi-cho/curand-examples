da4774ca3b8950384c4f4d6c6b45a2cc14b1d53e
Yong-Gwi Cho <yonggwi.cho@gmail.com>
2019-11-01 13:50:30 +0900

    added a python script.

---
+++ make_normal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import matplotlib.pyplot as plt
+import argparse as ae
+import math
+
+ierr = os.system("./test > output ")
+
+if ierr == 1 :
+    print("failed ./test execution.")
+    sys.exit(1)
+# read file
+f = open("output","r")
+data = f.readlines()
+for i in range(len(data)):
+    data[i] = float(data[i])
+f.close()
+
+# plot
+plt.hist(data,bins=100)
+plt.show()
